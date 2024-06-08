91733a66e9fd0f905daf38bd5dab7bd92380afca
Artem <artem_bondar98@mail.ru>
2024-06-08 14:34:25 +0200

    fix

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/telegram/webhook', [TelegramBotController::class, 'webhook']);
+Route::get('test',  function (Request $request) {
+    return ['ok'];
+});
